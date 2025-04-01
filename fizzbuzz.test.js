e55f1669733d810caa23a144bdb925ebcb1fa2b6
Jude Abraham <jude.abraham@epitech.eu>
2025-04-01 11:30:06 +0200

    test

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,6 +1,6 @@
 const test = require('node:test');
 const assert = require('node:assert');
-const { fizzbuzz } = require('./fizzbuzz.js').default
+const { fizzbuzz } = require('./fizzbuzz.js');
 
 test('fizzbuzz(3)', (t) => {
     assert.strictEqual(fizzbuzz(3), "Fizz");
